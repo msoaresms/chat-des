f8942cbb413823e14e97019b68c755fb20b55376
Matheus Soares <matheus.soares@itbam.org.br>
2021-06-30 12:14:25 -0400

    add chat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { breakPuzzle, generatePuzzles } from './shared/puzzle';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title = 'chat-des';
+  public chatForm = new FormGroup({
+    message: new FormControl('', [Validators.required]),
+  });
 
   ngOnInit() {
-    let {msgs, keys} = generatePuzzles();
-    breakPuzzle(msgs);
+    let { msgs, keys } = generatePuzzles();
+    // breakPuzzle(msgs);
 
     // testDES();
   }
