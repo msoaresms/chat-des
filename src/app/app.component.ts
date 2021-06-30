f3a3ad949f92a650923afc5cc92e80c61385f4d2
Matheus Soares <matheus.soares@itbam.org.br>
2021-06-30 17:35:34 -0400

    initial form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { IMqttMessage, MqttService } from 'ngx-mqtt';
 import { breakPuzzle, generatePuzzles } from './shared/puzzle';
 
 @Component({
@@ -8,14 +9,20 @@ import { breakPuzzle, generatePuzzles } from './shared/puzzle';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  public messages = [];
+
   public chatForm = new FormGroup({
     message: new FormControl('', [Validators.required]),
   });
 
-  ngOnInit() {
-    let { msgs, keys } = generatePuzzles();
-    // breakPuzzle(msgs);
+  constructor(private mqttService: MqttService) {}
 
+  ngOnInit() {
+    this.mqttService
+      .observe('messages')
+      .subscribe((message: IMqttMessage) => {});
+    // let { msgs, keys } = generatePuzzles();
+    // breakPuzzle(msgs);
     // testDES();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { IMqttServiceOptions, MqttModule } from 'ngx-mqtt';
 import { AppComponent } from './app.component';
 
+export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
+  hostname: 'localhost',
+  port: 1883,
+  path: '',
+};
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MqttModule.forRoot(MQTT_SERVICE_OPTIONS),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
